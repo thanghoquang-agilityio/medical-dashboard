706ab2467468431ff7de48e65497ac3eefddab1c
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2024-09-11 17:40:12 +0700

    Feat: implement themes for tailwind and next ui

---
+++ src/themes/colors.ts
@@ -0,0 +1,40 @@
+export const colors = {
+  black: '#000000',
+  'black-violet': {
+    light: '#1B1C31',
+    dark: '#e4e3ce',
+  },
+  blue: '#377DFF',
+  white: '#ffffff',
+  emerald: '#009D92',
+  gray: {
+    light: '#8F8F8F',
+    dark: '#707070',
+  },
+  green: '#38CB89',
+  neutral: {
+    light: '#1C1C1E',
+    dark: '#e3e3e1',
+  },
+  orange: '#FF5630',
+  purple: '#54577A',
+  'purple-shiny': {
+    light: '#222338',
+    dark: '#dddcc7',
+  },
+  red: '#D80027',
+  sky: '#15BFFD',
+  stone: {
+    light: '#D9D9D9',
+    dark: '#262626',
+  },
+  yellow: '#FFA600',
+};
+
+export const backgroundImage = {
+  'linear-banner':
+    'linear-gradient(66.15deg, #B2E6FD -5.25%, #38CB89 35.43%, #15BFFD 98.77%)',
+  'linear-button': 'linear-gradient(265.41deg, #46AA72 0%, #2DA0A4 77.48%)',
+  'linear-sidebar':
+    'linear-gradient(92.44deg, #15BFFD -3.59%, #00FF85 108.46%)',
+};
