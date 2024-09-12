698c87d437c9b5a20b76b9da09a142a71955d842
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2024-09-12 14:36:11 +0700

    Refactor: commit lint config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const DashboardPage = () => (
   <main className="container mx-auto">
-    <h1 className="text-lg font-semibold">Welcome to Medical Dasboard</h1>
+    <h1 className="text-lg font-semibold">Welcome to Medical Dashboard</h1>
   </main>
 );
 
