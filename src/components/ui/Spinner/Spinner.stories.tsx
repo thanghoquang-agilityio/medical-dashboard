84382f4a095ff7336be7a27eba0389c210179fa9
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2024-09-16 16:20:48 +0700

    feat: implement spinner component

---
+++ src/components/ui/Spinner/Spinner.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Spinner } from '.';
+
+const meta = {
+  title: 'Components/Spinner',
+  tags: ['autodocs'],
+
+  component: Spinner,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Spinner>;
+
+export default meta;
+type Story = StoryObj<typeof Spinner>;
+
+export const SpinnerSmall: Story = {
+  args: {
+    size: 'sm',
+  },
+};
+
+export const SpinnerMedium: Story = {
+  args: {
+    size: 'md',
+  },
+};
+
+export const SpinnerLarge: Story = {
+  args: {
+    size: 'lg',
+  },
+};
