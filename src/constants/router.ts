b18cb3cc78d3849727287f61aa799b362fd3bb69
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2024-09-20 16:19:43 +0700

    feat: implement style for dashboard

--- src/constants/router.ts
+++ src/constants/router.ts
@@ -1,7 +1,7 @@
 export const ROUTER = {
   LOGIN: '/login',
   SIGNUP: '/signup',
-  DASHBOARD: '/',
+  DASHBOARD: '/dashboard',
   CHEMISTS: '/chemists',
   APPOINTMENTS: '/appointments',
   FORGOT_PASSWORD: '/forgot-password',
