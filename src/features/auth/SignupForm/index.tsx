61adaf2b65a380802db9174a8ce8993287b67e92
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2024-09-20 18:01:28 +0700

    refactor: update style for signup form

--- src/features/auth/SignupForm/index.tsx
+++ src/features/auth/SignupForm/index.tsx
@@ -80,104 +80,109 @@ const SignupForm = () => {
   };
 
   return (
-    <div className="max-w-[528px] rounded-3xl shadow-lg flex flex-col items-center bg-primary-100 h-fit">
-      <Text customClass="font-bold text-secondary-300 text-3xl">Signup</Text>
+    <div className="w-full max-w-[528px] bg-background-100 flex flex-col justify-center items-center rounded-3xl py-6 lg:px-6 mx-2">
+      <Text variant="quaternary" size="3xl">
+        Signup
+      </Text>
       <form
-        className="w-full px-5 md:px-10 lg:px-[72px]"
+        className="flex flex-col md:px-10 px-4 pt-4 w-full"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div className="mt-8 flex flex-col gap-4 items-center">
-          <Controller
-            name="username"
-            control={control}
-            render={({
-              field: { onChange, name, ...rest },
-              fieldState: { error },
-            }) => (
-              <Input
-                {...rest}
-                size="lg"
-                placeholder="user name"
-                startContent={<DoctorIcon customClass={iconClass} />}
-                isInvalid={!!error?.message}
-                errorMessage={error?.message}
-                onChange={handleInputChange(name, onChange)}
-              />
-            )}
-            rules={SIGN_UP_FORM_VALIDATION.USERNAME}
-          />
-          <Controller
-            name="email"
-            control={control}
-            render={({
-              field: { onChange, name, ...rest },
-              fieldState: { error },
-            }) => (
-              <Input
-                {...rest}
-                size="lg"
-                placeholder="email address"
-                startContent={<EmailIcon customClass={iconClass} />}
-                isInvalid={!!error?.message}
-                errorMessage={error?.message}
-                onChange={handleInputChange(name, onChange)}
-              />
-            )}
-            rules={SIGN_UP_FORM_VALIDATION.EMAIL}
-          />
+        <Controller
+          name="username"
+          control={control}
+          render={({
+            field: { onChange, name, ...rest },
+            fieldState: { error },
+          }) => (
+            <Input
+              {...rest}
+              size="lg"
+              placeholder="user name"
+              className="py-2"
+              startContent={<DoctorIcon customClass={iconClass} />}
+              isInvalid={!!error?.message}
+              errorMessage={error?.message}
+              onChange={handleInputChange(name, onChange)}
+            />
+          )}
+          rules={SIGN_UP_FORM_VALIDATION.USERNAME}
+        />
+        <Controller
+          name="email"
+          control={control}
+          render={({
+            field: { onChange, name, ...rest },
+            fieldState: { error },
+          }) => (
+            <Input
+              {...rest}
+              size="lg"
+              placeholder="email address"
+              className="py-2"
+              startContent={<EmailIcon customClass={iconClass} />}
+              isInvalid={!!error?.message}
+              errorMessage={error?.message}
+              onChange={handleInputChange(name, onChange)}
+            />
+          )}
+          rules={SIGN_UP_FORM_VALIDATION.EMAIL}
+        />
 
-          <Controller
-            name="password"
-            control={control}
-            render={({
-              field: { onChange, name, ...rest },
-              fieldState: { error },
-            }) => (
-              <Input
-                {...rest}
-                size="lg"
-                type="password"
-                placeholder="password"
-                startContent={<LockIcon customClass={iconClass} />}
-                isInvalid={!!error?.message}
-                errorMessage={error?.message}
-                onChange={handleInputChange(name, onChange)}
-              />
-            )}
-            rules={SIGN_UP_FORM_VALIDATION.PASSWORD}
-          />
+        <Controller
+          name="password"
+          control={control}
+          render={({
+            field: { onChange, name, ...rest },
+            fieldState: { error },
+          }) => (
+            <Input
+              {...rest}
+              size="lg"
+              type="password"
+              placeholder="password"
+              className="py-2"
+              startContent={<LockIcon customClass={iconClass} />}
+              isInvalid={!!error?.message}
+              errorMessage={error?.message}
+              onChange={handleInputChange(name, onChange)}
+            />
+          )}
+          rules={SIGN_UP_FORM_VALIDATION.PASSWORD}
+        />
 
-          <Controller
-            name="confirmPassWord"
-            control={control}
-            render={({
-              field: { onChange, name, ...rest },
-              fieldState: { error },
-            }) => (
-              <Input
-                {...rest}
-                size="lg"
-                type="password"
-                placeholder="confirm password"
-                startContent={<LockIcon customClass={iconClass} />}
-                isInvalid={!!error?.message}
-                errorMessage={error?.message}
-                onChange={handleInputChange(name, onChange)}
-              />
-            )}
-            rules={SIGN_UP_FORM_VALIDATION.CONFIRM_PASSWORD}
-          />
+        <Controller
+          name="confirmPassWord"
+          control={control}
+          render={({
+            field: { onChange, name, ...rest },
+            fieldState: { error },
+          }) => (
+            <Input
+              {...rest}
+              size="lg"
+              type="password"
+              placeholder="confirm password"
+              className="py-2"
+              startContent={<LockIcon customClass={iconClass} />}
+              isInvalid={!!error?.message}
+              errorMessage={error?.message}
+              onChange={handleInputChange(name, onChange)}
+            />
+          )}
+          rules={SIGN_UP_FORM_VALIDATION.CONFIRM_PASSWORD}
+        />
 
-          <Button
-            type="submit"
-            isDisabled={!isValid || !isDirty}
-            isLoading={isLoading}
-            className="w-full py-2 text-lg"
-          >
-            Signup
-          </Button>
-        </div>
-        <div className="flex my-8 justify-center gap-3">
+        <Button
+          type="submit"
+          size="lg"
+          isDisabled={!isValid || !isDirty}
+          isLoading={isLoading}
+          className="mt-8"
+        >
+          Signup
+        </Button>
+        <div className="flex justify-center w-full gap-6 pt-10 pb-3">
           <Text>Already have account?</Text>
           <Link
             href={ROUTER.LOGIN}
