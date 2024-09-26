e6ca59917e62082e8809beab0a8ff61d4583bf8b
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2024-09-26 15:17:11 +0700

    feat: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self' https://fonts.gstatic.com;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;`;
+
 const nextConfig = {
+  swcMinify: true,
+  experimental: {
+    optimizePackageImports: ['tailwind-merge', 'react-hook-form'],
+  },
   images: {
     remotePatterns: [
       {
@@ -9,6 +26,25 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=86400; includeSubDomains;',
+          },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+        ],
+      },
+    ];
   },
 };
 
