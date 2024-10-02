fbf081fa220d4f832ae2a5e8d65bf42788c51601
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2024-10-02 14:19:56 +0700

    feat: test commit

--- src/app/(dashboard)/chemists/page.tsx
+++ src/app/(dashboard)/chemists/page.tsx
@@ -1,5 +1,7 @@
 const ChemistPage = () => (
-  <h1 className="text-lg font-semibold">Welcome to Chemist page</h1>
+  <h1 className="text-lg font-semibold">
+    Welcome to Chemist page: test commit
+  </h1>
 );
 
 export default ChemistPage;
