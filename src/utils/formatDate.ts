aeb87ef6c355ee5baa93ee6f65692db22f6f5b86
Thao Ha <thao.ha@asnet.com.vn>
2024-10-04 10:06:28 +0700

    feat: update option for duration time field

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,3 +1,4 @@
+import { Option } from '@/types';
 import dayjs from 'dayjs';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -112,4 +113,24 @@ export const isLaterThanCurrentTime = (date: string) => {
   const currentDate = new Date();
 
   return inputDate < currentDate;
+};
+
+export const generateTimeOptions = (): Option[] => {
+  const times: Option[] = [];
+
+  for (let hour = 0; hour < 24; hour++) {
+    for (let minutes = 0; minutes < 60; minutes += 15) {
+      // Format the hours and minutes to be two digits
+      const hourStr = hour.toString().padStart(2, '0');
+      const minuteStr = minutes.toString().padStart(2, '0');
+
+      // Push the new object into the time array
+      times.push({
+        key: `${hourStr}:${minuteStr}`,
+        label: `${hourStr}:${minuteStr}`,
+      });
+    }
+  }
+
+  return times;
 };
