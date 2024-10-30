5661d07c18363ebe017cf5f30cde178025fc1a58
Thao Ha <thao.ha@asnet.com.vn>
2024-10-30 16:58:25 +0700

    feat: apply security

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self' https://fonts.gstatic.com;
+    connect-src 'self' https://dev.perlinks.com;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;`;
+
 const nextConfig = {
+  swcMinify: true,
+  experimental: {
+    optimizePackageImports: ['react-hook-form'],
+  },
+  reactStrictMode: false,
+  staticPageGenerationTimeout: 1000,
+
   images: {
     remotePatterns: [
       {
@@ -8,7 +29,7 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
-      // TODO: update config images later
+      // TODO: this code to run in local, will remove later
       {
         protocol: 'http',
         hostname: '**',
@@ -18,6 +39,25 @@ const nextConfig = {
         hostname: 'i.ibb.co',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=86400; includeSubDomains;',
+          },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+        ],
+      },
+    ];
   },
 };
 export default nextConfig;
