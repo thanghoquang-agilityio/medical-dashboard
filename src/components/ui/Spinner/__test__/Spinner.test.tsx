13a1dea466c2d743314a0f5034a797cf27e8f322
huy.phamnhat <huy.phamnhat@asnet.com.vn>
2024-11-08 17:49:18 +0700

    test: update unit test for all components

--- src/components/ui/Spinner/__test__/Spinner.test.tsx
+++ src/components/ui/Spinner/__test__/Spinner.test.tsx
@@ -2,11 +2,28 @@ import { render } from '@testing-library/react';
 
 // Component
 import { Spinner } from '..';
+import { SpinnerProps } from '@nextui-org/react';
 
 describe('Spinner Component', () => {
-  it('should render Spinner component correctly', () => {
-    const { container } = render(<Spinner />);
+  const setup = (props: SpinnerProps) => render(<Spinner {...props} />);
 
-    expect(container).toMatchSnapshot();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it('should render Spinner component correctly without props', () => {
+    const { asFragment } = setup({});
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render Spinner component correctly with props', () => {
+    const { asFragment } = setup({
+      classNames: {
+        circle1: '',
+        circle2: '',
+      },
+    });
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });
