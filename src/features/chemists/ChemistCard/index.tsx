ed0d2e0619abd96aee547ee89a6f5798684e6d3d
Thao Ha <thao.ha@asnet.com.vn>
2024-11-15 17:51:37 +0700

    feat: install snapshot

--- src/features/chemists/ChemistCard/index.tsx
+++ src/features/chemists/ChemistCard/index.tsx
@@ -135,14 +135,6 @@ const ChemistCard = ({ id, data, isAdmin, onEdit }: ChemistCardProps) => {
                 )}
               </div>
             </div>
-            {!isAdmin && (
-              <Button
-                color="default"
-                className="text-secondary-500 text-lg font-medium p-0 min-w-10"
-              >
-                Book
-              </Button>
-            )}
           </CardHeader>
           <CardBody className="p-0">
             <Text
